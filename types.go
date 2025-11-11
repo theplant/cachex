a08600502bb8970a3345a30aefab9191c1587e74
molon <3739161+molon@users.noreply.github.com>
2025-11-11 18:43:58 +0800

    Fix indentation in State enum constants

--- types.go
+++ types.go
@@ -9,8 +9,8 @@ type State int8
 
 const (
 	StateFresh    State = iota // Data is fresh and valid
-	StateStale                  // Data is stale but usable
-	StateTooStale               // Data is too stale and must be refreshed
+	StateStale                 // Data is stale but usable
+	StateTooStale              // Data is too stale and must be refreshed
 )
 
 // Upstream defines the interface for a data source that can retrieve values
